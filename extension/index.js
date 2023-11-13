703463d88702a1aba60697466e5f875510fd51ef
FikirYibeltal <fikiryibeltal09@gmail.com>
2023-11-13 23:35:48 +0400

    fix: clear issues

--- extension/index.js
+++ extension/index.js
@@ -3,15 +3,18 @@ const button = document.getElementById('decompress');
 button.addEventListener('click', () => {
  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
  const activeTab = tabs[0];
- console.log(tabs);
+ console.log(tab);
  chrome.tabs.sendMessage(activeTab.id, { action: 'setLocalStorage', data: { key: 'decompress', value: 'true' } });
 });
+
+});
+
 const clearButton = document.getElementById('clear'); 
 clearButton.addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const activeTab = tabs[0];
-    console.log(tabs);
-    chrome.tabs.sendMessage(activeTab.id, { action: 'clear', data: { } });
-   });
+ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+ const activeTab = tabs[0];
+ console.log(tab);
+ chrome.tabs.sendMessage(activeTab.id, { action: 'clear', data: { } });
+});
 
 });
